e1fb2e5fa0a9daa3753dfd1e32d9534c77404eae
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-11-24 16:14:23 +0100

    feat(Steves): Added dark & light button - WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React, { useState } from 'react'
+import './App.css'
+import React, { useState, useEffect } from 'react'
 import WordInputSearch from './components/WordInputSearch/WordInputSearch'
 import WordCard from './Components/WordCard/WordCard'
+
 function App() {
   const [data, setData] = useState(null)
+  const [darkMode, setDarkMode] = useState(false)
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode)
+  }
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-mode', darkMode)
+  }, [darkMode])
 
   return (
     <div>
+      <button className="dark-mode-btn" onClick={toggleDarkMode}>
+        Switch to {darkMode ? 'light' : 'dark'} mode
+      </button>
       <WordInputSearch setData={setData} />
       {Array.isArray(data) &&
         data.map((wordData, index) => (
